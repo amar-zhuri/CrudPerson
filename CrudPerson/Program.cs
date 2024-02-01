13a7a1c237476d8d9aba0ab862d637b4916c59d3
amar-zhuri <amarzhurix@gmail.com>
2024-02-01 14:24:23 +0100

    Add project files.

---
+++ CrudPerson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<Person> persons = new List<Person>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Create");
+            Console.WriteLine("2. Read");
+            Console.WriteLine("3. Update");
+            Console.WriteLine("4. Delete");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            break;
+
+        }
+    }
+
+    class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+
+
+
+
+
+
+
+
+}
